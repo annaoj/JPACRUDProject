500115c1921397dc8a04b9a7896568d35d1efc46
Anna Jimenez <annajimenez146@gmail.com>
2019-03-01 16:01:22 -0700

    connect boot and jpa projects

---
+++ BootMVCOnlinestore/src/main/java/com/skilldistillery/onlinestore/BootMvcOnlinestoreApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcOnlinestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcOnlinestoreApplication.class, args);
+	}
+
+}
